f82c91b34b7f73637837a76ea41be4e6d74a9dcd
gogu <gogu9909@gmail.com>
2022-05-24 10:49:54 +0800

    chore: Prepare for dev.

--- src/components/MainView/index.js
+++ src/components/MainView/index.js
@@ -1,5 +1,6 @@
-import { DataView as DataViewOld } from "./DataViewOld/Table";
+// import { DataView as DataViewOld } from "./DataViewOld/Table";
 import { DataView as DataViewNew } from "./DataView/DataView";
-import { FF_DEV_1470, isFF } from "../../utils/feature-flags";
+// import { FF_DEV_1470, isFF } from "../../utils/feature-flags";
 
-export const DataView = isFF(FF_DEV_1470) ? DataViewNew : DataViewOld;
+// export const DataView = isFF(FF_DEV_1470) ? DataViewNew : DataViewOld;
+export const DataView = DataViewNew;
